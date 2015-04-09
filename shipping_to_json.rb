7a624924cd40b94a6967a162df9b088d2393706a
Brian Stolte <brian.stolte@indiegogo.com>
2015-04-09 14:12:00 -0700

    updated with comments, cleaned up a puts

--- shipping_to_json.rb
+++ shipping_to_json.rb
@@ -1,8 +1,6 @@
 # Takes a tab separated CSV file with country codes and prices and outputs 
 # a json file for use with perks
 
-
-
 require "csv"
 require "json"
 
@@ -10,27 +8,23 @@ parsed_file = CSV.read("data.tsv", { :col_sep => "\t", :headers => true})
 
 perks = []
 
+# starts on the 3rd column (1st perk) and iterates through the columns
+# filling up the hash with the perk name and the country code and prices 
 2.upto(parsed_file.first.length-1) do |i|
 	perk = {
 		name: parsed_file.headers[i],
 		fees: {}
 	}
-		parsed_file.each do |row|
-				
+		parsed_file.each do |row|	
 			if row[i] != nil
-
 				perk[:fees][row["Country Code"]] = row[i]
-
 			end
-
 		end
 
 	perks.push(perk)
-
 end
 
-puts perks.to_json
-
+# writes the output to a json file 
 File.open("temp.json","w") do |f|
   f.write(perks.to_json)
 end
